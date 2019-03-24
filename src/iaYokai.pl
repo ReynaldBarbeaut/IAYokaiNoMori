a22cfbe17acc2ddd37324bfae00cfa6ccc607539
Spoutnissou <reynald.barbeaut@gmail.com>
2019-03-24 19:08:52 +0100

    Ajout de prédicats

--- src/iaYokai.pl
+++ src/iaYokai.pl
@@ -1,5 +1,3 @@
-:-use_module(library(plunit)).
-:-use_module(library(lists)).
 /*Barbeaut Reynald - Bouchard Nicolas
 *
 * This file contains the IA of a player of Yokai no-mori
@@ -7,6 +5,15 @@
 *
 *
 */
+
+:-use_module(library(clpfd)).
+:-use_module(library(plunit)).
+:-use_module(library(lists)).
+
+% Flag to display a long list
+:-set_prolog_flag(toplevel_print_options,
+    [quoted(true), portrayed(true), max_depth(0)]).
+
 
 /*
 *       Initialisation of the board
@@ -63,13 +70,136 @@ correctSquare([X,Y]) :- X > 0, X < 6, Y > 0, Y < 6.
 /*
 * Test if a move is correct
 */
-correctMove(piece(south,_,_),C2,Board) :-
+correctMove(piece(south,_,C1),C2,Board) :-
+        C1 \= C2,
         \+(member(piece(south, _, C2), Board)),
         correctSquare(C2).
 
-correctMove(piece(north,_,_),C2,Board) :-
+correctMove(piece(north,_,C1),C2,Board) :-
+        C1 \= C2,
         \+(member(piece(north, _, C2), Board)),
         correctSquare(C2).
 
+/*
+*  Move pieces
+*/
 
+/*
+* Move on sides (common move for both sides)
+*/
+movePiece(piece(_,Name,[X,Y]),piece(north,Name,[X2,Y]),Board) :-
+    Name \= kodama,
+    Name \= oni,
+    X2 is X + 1,
+    correctMove(piece(_,Name,[X,Y]),[X2,Y],Board).
+
+movePiece(piece(north,Name,[X,Y]),piece(north,Name,[X2,Y]),Board) :-
+    Name \= kodama,
+    Name \= oni,
+    X2 is X - 1,
+    correctMove(piece(north,Name,[X,Y]),[X2,Y],Board).
+
+/*
+* Move a north piece
+*/
+
+/*
+* Move to the front
+*/
+movePiece(piece(north,_,[X,Y]),piece(north,_,[X,Y2]),Board) :-
+    Y2 is Y + 1,
+    correctMove(piece(north,_,[X,Y]),[X,Y2],Board).
+
+/*
+* Move to the front left and front right
+*/
+movePiece(piece(north,Name,[X,Y]),piece(north,Name,[X2,Y2]),Board) :-
+    Name \= kodama,
+    Y2 is Y + 1,
+    X2 is X + 1,
+    correctMove(piece(north,Name,[X,Y]),[X2,Y2],Board).
+
+movePiece(piece(north,Name,[X,Y]),piece(north,Name,[X2,Y2]),Board) :-
+    Name \= kodama,
+    Y2 is Y + 1,
+    X2 is X - 1,
+    correctMove(piece(north,Name,[X,Y]),[X2,Y2],Board).
+
+/*
+* Move behind
+*/
+movePiece(piece(north,Name,[X,Y]),piece(north,Name,[X,Y2]),Board) :-
+    Name \= kodama,
+    Name \= oni,
+    Y2 is Y - 1,
+    correctMove(piece(north,Name,[X,Y]),[X,Y2],Board).
+
+
+/*
+* Move to the back left and back right
+*/
+movePiece(piece(north,Name,[X,Y]),piece(north,Name,[X2,Y2]),Board) :-
+    (Name=oni;Name=koropokkuru),
+    Y2 is Y - 1,
+    X2 is X + 1,
+    correctMove(piece(north,Name,[X,Y]),[X2,Y2],Board).
+
+movePiece(piece(north,Name,[X,Y]),piece(north,Name,[X2,Y2]),Board) :-
+    (Name=oni;Name=koropokkuru),
+    Y2 is Y - 1,
+    X2 is X - 1,
+    correctMove(piece(north,Name,[X,Y]),[X2,Y2],Board).
+
+/*
+* Move a south piece
+*/
+
+/*
+* Move to the front
+*/
+movePiece(piece(south,_,[X,Y]),piece(south,_,[X,Y2]),Board) :-
+    Y2 is Y - 1,
+    correctMove(piece(south,_,[X,Y]),[X,Y2],Board).
+
+/*
+* Move to the front left and front right
+*/
+movePiece(piece(south,Name,[X,Y]),piece(south,Name,[X2,Y2]),Board) :-
+    Name \= kodama,
+    Y2 is Y - 1,
+    X2 is X + 1,
+    correctMove(piece(south,Name,[X,Y]),[X2,Y2],Board).
+
+movePiece(piece(south,Name,[X,Y]),piece(south,Name,[X2,Y2]),Board) :-
+    Name \= kodama,
+    Y2 is Y - 1,
+    X2 is X - 1,
+    correctMove(piece(south,Name,[X,Y]),[X2,Y2],Board).
+
+/*
+* Move behind
+*/
+movePiece(piece(south,Name,[X,Y]),piece(south,Name,[X,Y2]),Board) :-
+    Name \= kodama,
+    Name \= oni,
+    Y2 is Y + 1,
+    correctMove(piece(south,Name,[X,Y]),[X,Y2],Board).
+
+
+/*
+* Move to the back left and back right
+*/
+movePiece(piece(south,Name,[X,Y]),piece(south,Name,[X2,Y2]),Board) :-
+    (Name=oni;Name=koropokkuru),
+    Y2 is Y + 1,
+    X2 is X + 1,
+    correctMove(piece(south,Name,[X,Y]),[X2,Y2],Board).
+
+movePiece(piece(south,Name,[X,Y]),piece(south,Name,[X2,Y2]),Board) :-
+    (Name=oni;Name=koropokkuru),
+    Y2 is Y + 1,
+    X2 is X - 1,
+    correctMove(piece(south,Name,[X,Y]),[X2,Y2],Board).
+                                                                         
+                                               
 
