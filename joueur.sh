e20dcdafeaf8d5637451affc10699d016bc624d4
Nicolas Bouchard <nicolas.bouchard@icloud.com>
2019-05-06 15:17:10 +0200

    modif bool term

---
+++ joueur.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# joueur.sh
+
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 hostServeur portServeur nomJoueur sensTete" >&2
+  exit 1
+fi
+
+make
+cd ./src
+make
+cd ../
+echo "coucou"
+./bin/player $1 $2 $3 $4
+
+java -Djava.library.path="/usr/local/sicstus4.4.1/bin/jasper.jar" -classpath ./jasper/jasper.jar:. ./src/IAClient $1 $2
+
