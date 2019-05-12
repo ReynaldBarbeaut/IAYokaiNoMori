e602fbdb1e1aa2b094dee620409d576931be8f71
Nicolas Bouchard <nicolas.bouchard@icloud.com>
2019-05-12 20:20:54 +0200

    amélioration script

--- joueur.sh
+++ joueur.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # joueur.sh
 
+PORT_IA=3708
+
 if [ "$#" -ne 3 ]; then
   echo "Usage: $0 hostServeur portServeur nomJoueur" >&2
   exit 1
@@ -9,4 +11,4 @@ fi
 make
 cd ./src
 make
-../bin/player $1 $2 3708 $3 & java -Djava.library.path="../lib" -classpath ../lib/sicstus-4.4.1/bin/jasper.jar:. IAClient $1 3708 && fg
+../bin/player $1 $2 $PORT_IA $3 & java -Djava.library.path="../lib" -classpath ../lib/sicstus-4.4.1/bin/jasper.jar:. IAClient $1 $PORT_IA && fg
