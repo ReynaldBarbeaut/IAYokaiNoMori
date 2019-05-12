bbbc30107c17d4cd816179574aad332b9970810c
Sptounissou <reynald.barbeaut@gmail.com>
2019-05-12 20:36:40 +0200

    Bonne lib

--- lib/sicstus-4.4.1/library/license.pl
+++ lib/sicstus-4.4.1/library/license.pl
@@ -2,4 +2,4 @@
 % Transferring this file to third parties is an act of software piracy.
 
 site('studentSP4.4univ-fcomte.fr').
-product('sicstus4.4_linux', 'permanent', 'e259-a5b9-rh8s-bssg-sqy6').
+product('sicstus4.4_linux', 'permaent', 'e259-a5b9-rh8s-bssg-sqy6').
